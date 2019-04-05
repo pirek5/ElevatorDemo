0aabc68bc138c18229658256f671d2d5ac977d29
pirek5 <pir.czajkowski@gmail.com>
2019-04-05 16:09:58 +0200

    bug fixes

---
+++ Assets/ElevatorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+    //set in editor
+    [SerializeField] private Transform elevator;
+    [SerializeField] private Vector3[] floorPositions;
+
+    //state
+    int currentFloor;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Photocell.cs
+++ Assets/Scripts/Photocell.cs
@@ -8,11 +8,26 @@ public class Photocell : MonoBehaviour
     //dependencies
     [Inject] private DoorController doorController;
 
-    private void OnTriggerEnter(Collider collider)
+    //state
+    private bool photocellEnabled;
+
+    private void OnTriggerStay(Collider collider)
     {
-        if (collider.GetComponent<PlayerActions>()) // auto opening works only for player, more universal aproach would be using for example layers or tags
+        if (collider.GetComponent<PlayerActions>() && photocellEnabled) // auto opening works only for player, more universal aproach would be using for example layers or tags
         {
             doorController.AutoOpenDoor();
+            photocellEnabled = false;
         }
     }
+
+    public void EnablePhotocell()
+    {
+        photocellEnabled = true;
+    }
+
+    public void DisablePhotocell()
+    {
+        photocellEnabled = false;
+    }
+    
 }
